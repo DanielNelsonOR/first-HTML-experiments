04acea3a9ab05226534839127773112e26b4fee3
DanielNelsonOR <dnelson2542@gmail.com>
2021-01-31 10:29:04 -0800

    Add user functionality

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,28 @@ myImage.onclick = function() {
         myImage.setAttribute('src','images/puppy.jpeg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+        setUserName();
+    } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Puppies are cool, ' + myName;
+    }
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Puppies are cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+
